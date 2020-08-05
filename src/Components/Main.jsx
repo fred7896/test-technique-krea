2e7403ded7ee00330d0049df4c23ed18c0383699
FredericMentel <frederic.mentel@gmail.com>
2020-08-05 18:29:13 +0200

    Edito Section Added.

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -61,7 +61,7 @@ export default class Main extends React.Component {
                 <div id="content">
                     <div className="container-header-img">
                         <img src={visu} width="100%" alt="logo distingo" className="visu" />
-                        <div className="group-title mt-4 py-3">
+                        <div className="group-title py-3">
                             <h1 className="period py-1">DU 6 MAI AU 6 JUIN 2020</h1>
                             <div className="big-game py-1">
                                 <h2 style={{ fontSize: "inherit" }}>GRAND JEU</h2>
@@ -72,6 +72,13 @@ export default class Main extends React.Component {
                             </div>
                             <p className="test-app">Et si vous testiez notre app ?</p></div>
                     </div>
+                    <div className="container-edito" >
+                        <section id="edito" className="p-4">
+                            <p id="last-step">Plus qu'une étape pour valider votre participation !</p>
+                            <p id="invite-form">Pour valider votre participation au tirage au sort et vous sauvez pour de bon,<br /> veuillez remplir les champs ci-dessous :</p>
+                        </section>
+                    </div>
+
                     <div className="container-form">
                         <form className="p-3" onSubmit={this.handleSubmit}>
 
