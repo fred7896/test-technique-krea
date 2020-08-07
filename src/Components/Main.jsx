71c5f28514904ebae692959204ce28c9fc41d579
FredericMentel <frederic.mentel@gmail.com>
2020-08-07 03:25:58 +0200

    Form errors management.

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -17,17 +17,26 @@ export default class Main extends React.Component {
             birthDate: "",
             email: "",
             phoneNumber: "",
-            inputCheckbox: false
+            inputCheckbox: false,
+            isSubmit: false,
+            isNameValid: false,
+            isSurnameValid: false,
+            isValidBirthDate: false,
+            isEmail: false
+
         }
     }
+
+
 
     handleChange = (e) => {
         const name = e.target.name;
         const type = e.target.type;
-        const value = type === "checkbox" ? e.target.checked : e.target.value
+        let value = type === "checkbox" ? e.target.checked : e.target.value;
         this.setState({
-            [name]: value
-        })
+            [name]: value,
+            isSubmit: false
+        }, this.controlForm)
     }
 
     handleCivilite = (e) => {
@@ -36,9 +45,51 @@ export default class Main extends React.Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
+        this.setState({
+            isSubmit: true
+        }, this.checkData);
+    }
+
+    checkData = () => {
         const data = JSON.stringify(this.state);
-        console.log(data);
-        this.resetField();
+        const { isNameValid, isSurnameValid, isValidBirthDate, isEmail, phoneNumber } = this.state;
+        if (isNameValid && isSurnameValid && isValidBirthDate && isEmail && phoneNumber.length > 0) {
+            console.log(data);
+        } else {
+            console.log("le formulaire contient des erreurs");
+        }
+    }
+
+    checkName = () => {
+        const regexp = /^[a-zA-Z-'. ]+$/;
+        let isNameValid = regexp.test(this.state.customerName);
+        let isSurnameValid = regexp.test(this.state.customerSurname);
+        this.setState({
+            isNameValid: isNameValid,
+            isSurnameValid: isSurnameValid
+        })
+    }
+
+    checkBirth = (birthDate) => {
+        const regexp = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/;
+        let isValidBirthDate = regexp.test(birthDate);
+        this.setState({
+            isValidBirthDate: isValidBirthDate
+        })
+    }
+
+    checkEmail = (email) => {
+        const regexp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        let isEmail = regexp.test(email);
+        this.setState({
+            isEmail: isEmail
+        })
+    }
+
+    controlForm = () => {
+        this.checkName();
+        this.checkBirth(this.state.birthDate);
+        this.checkEmail(this.state.email);
     }
 
     resetField = () => {
@@ -81,7 +132,7 @@ export default class Main extends React.Component {
                     </div>
 
                     <div className="container-form">
-                        <form className="p-3" onSubmit={this.handleSubmit}>
+                        <form className="p-3" onSubmit={this.handleSubmit} noValidate>
 
                             <div className="civ">
                                 <p className="pr-4" >Civilité*</p>
@@ -98,10 +149,19 @@ export default class Main extends React.Component {
 
                             <div>
                                 <Field name="customerName" type="text" value={this.state.customerName} onChange={this.handleChange} placeholder="Nom*" ></Field>
+                                {this.state.isSubmit && (this.state.customerName.length === 0) && <div className="error" aria-live="polite">Veuillez remplir ce champ</div>}
+                                {this.state.isSubmit && (this.state.customerName.length > 0) && !this.state.isNameValid && <div className="error" aria-live="polite">Nom invalide</div>}
                                 <Field name="customerSurname" type="text" value={this.state.customerSurname} onChange={this.handleChange} placeholder="Prénom*"></Field>
+                                {this.state.isSubmit && (this.state.customerSurname.length === 0) && <div className="error" aria-live="polite">Veuillez remplir ce champ</div>}
+                                {this.state.isSubmit && (this.state.customerSurname.length > 0) && !this.state.isSurnameValid && <div className="error" aria-live="polite">Prénom invalide</div>}
                                 <Field name="birthDate" type="text" value={this.state.birthDate} onChange={this.handleChange} placeholder="Date de naissance*"></Field>
+                                {this.state.isSubmit && (this.state.birthDate.length > 0) && !this.state.isValidBirthDate && <div className="error" aria-live="polite">Date de naissance invalide.<br />Formats acceptés : JJ/MM/AAAA ou JJ-MM-AAAA</div>}
+                                {this.state.isSubmit && (this.state.birthDate.length === 0) && <div className="error" aria-live="polite">Veuillez remplir ce champ</div>}
                                 <Field name="email" type="email" value={this.state.email} onChange={this.handleChange} placeholder="Adresse email*"></Field>
+                                {this.state.isSubmit && (this.state.email.length === 0) && <div className="error" aria-live="polite">Veuillez remplir ce champ</div>}
+                                {this.state.isSubmit && (this.state.email.length > 0) && !this.state.isEmail && <div className="error" aria-live="polite">Email invalide</div>}
                                 <Field name="phoneNumber" type="tel" value={this.state.phoneNumber} onChange={this.handleChange} placeholder="Téléphone*"></Field>
+                                {this.state.isSubmit && (this.state.phoneNumber.length === 0) && <div className="error" aria-live="polite">Veuillez remplir ce champ</div>}
                             </div>
                             <Checkbox name="inputCheckbox" value={this.state.inputCheckbox} onChange={this.handleChange} >J'accepte de recevoir les offres DISTINGO (notamment les offres de fidélisation réservées aux clients) ainsi que celles des sociétés du groupe PSA et de leur réseau de distribution par email.</Checkbox>
                             <div className="d-flex justify-content-center my-5"><input type="submit" value="VALIDER" className="button-validation"></input></div>
